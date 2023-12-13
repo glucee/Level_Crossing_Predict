d659b48ec9fb7873874ff81beecea88b5d905fa0
glucee <7841841+glucee@users.noreply.github.com>
2023-12-13 16:22:58 +0000

    Update levelcrossing.py
    Add one case: if now is the waiting time, predict when the waiting time will be end

--- levelcrossing.py
+++ levelcrossing.py
@@ -6,21 +6,21 @@ import urllib.request
 stations = [("bns", "Barnes"),   #train station name, you can find it at
             ("mtl", "Mortlake")] #https://raw.githubusercontent.com/jpsingleton/Huxley2/master/station_codes.csv
 Access_Token = "" #You will need to add your access token
-wait_window = 2 # the waiting time windows in minutes 
+wait_window = 2 # the window time for waiting in minutes 
 
 # check the next three trains between station 0 to station 1
 URLs=[ "https://huxley2.azurewebsites.net/departures/"+stations[0][0]+"/to/"+stations[1][0]+"/3/?accessToken="+Access_Token,
 "https://huxley2.azurewebsites.net/departures/"+stations[1][0]+"/to/"+stations[0][0]+"/3/?accessToken="+Access_Token]
 
-time_table = [] # store the departure times between two stations
+time_table = [] # store the current time and departure times between two stations
 window_flag = [] # check if two trains are in the same waiting windows: 1 in, 0: not
 time_format = "%H:%M"
 current_time = datetime.now().replace(second=0, microsecond=0)
-
+time_table.append(current_time) # please we add the current time in the time_table
 
 for url in URLs:
-    # response = open_url(url) # for PyScript
-    response = urllib.request.urlopen(url)
+    response = open_url(url) # for PyScript
+    # response = urllib.request.urlopen(url)
     trains = json.load(response)["trainServices"]
     for train in trains:
         departure = datetime.strptime(train["std"], time_format)
@@ -37,15 +37,18 @@ for i in range(0, len(time_table)-1):
         window_flag.append(0)
 window_flag.append(0)
 
-print("Predicted Closed Time: {} - {}".format(stations[0][1], stations[1][1]))
-if time_table[0] - current_time <= timedelta(minutes=wait_window):
-    print(current_time.time(), (time_table[0]+timedelta(minutes=1)).time())
+print("Current Time: " + current_time.strftime(time_format))
+print("Next Wait Periods: {} - {}".format(stations[0][1], stations[1][1]))
 
 index = 0
 while (index < len(time_table)):
     begin = time_table[index]
     while (window_flag[index] == 1):
-        index += 1
+        index += 1        
     end = time_table[index] + timedelta(minutes=wait_window/2)
+    if index != 0: # except the current time
+        if begin == current_time:
+            print("NOW - " + end.strftime(time_format))
+        else:
+            print(begin.strftime(time_format) + "-" + end.strftime(time_format))
     index += 1
-    print(begin.time(), end.time())
